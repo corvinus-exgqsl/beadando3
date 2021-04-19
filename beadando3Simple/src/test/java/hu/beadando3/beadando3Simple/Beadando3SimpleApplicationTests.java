676936c55d1594ad64de085996b8a7ea18462013
corvinus-exgqsl <80127143+corvinus-exgqsl@users.noreply.github.com>
2021-04-19 13:38:20 +0200

    Add files via upload

---
+++ beadando3Simple/src/test/java/hu/beadando3/beadando3Simple/Beadando3SimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.beadando3.beadando3Simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beadando3SimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
